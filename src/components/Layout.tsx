c640db0a95eb619955f332dbefc931e3aaec54bf
Devookim <khwoo8026@gmail.com>
2022-08-06 01:08:10 +0900

    feat: logo action

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { useCallback } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import AuthButton from "./AuthButton";
 
-const Layout = () => (
-    <div id="container" className="w-full h-screen">
-        <div
-            id="header"
-            className="w-full flex justify-between items-center h-12 border-2"
-        >
-            <div className="select-none">Wanted Challenge Fe1</div>
-            <AuthButton />
+const Layout = () => {
+    const navigate = useNavigate();
+
+    const onClickLogo = useCallback(() => navigate("/"), []);
+
+    return (
+        <div id="container" className="w-full h-screen">
+            <div
+                id="header"
+                className="w-full flex justify-between items-center h-12 border-2"
+            >
+                <div
+                    className="select-none cursor-pointer"
+                    onClick={onClickLogo}
+                >
+                    Wanted Challenge Fe1
+                </div>
+                <AuthButton />
+            </div>
+            <Outlet />
         </div>
-        <Outlet />
-    </div>
-);
+    );
+};
 
 export default Layout;
