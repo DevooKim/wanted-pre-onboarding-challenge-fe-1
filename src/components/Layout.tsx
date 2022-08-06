1ad3300fde7a76f87bc972a23aa58f43d30c13a4
Devookim <khwoo8026@gmail.com>
2022-08-06 15:26:45 +0900

    feat: layout;

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,19 +6,27 @@ const Layout = () => {
         <div id="container" className="w-full h-screen">
             <div id="header" className="justify-between border-2 navbar">
                 <div className="shrink-0">
-                    <div className="select-none">Wanted Challenge Fe1</div>
+                    <div className="text-xl normal-case select-none">
+                        Wanted Challenge Fe1
+                    </div>
                 </div>
                 <div className="px-6 shrink-0">
                     <div className="flex gap-4">
-                        <Link to="/">HOME</Link>
-                        <Link to="/todo">TO DO</Link>
+                        <Link className="text-xl normal-case" to="/">
+                            HOME
+                        </Link>
+                        <Link className="text-xl normal-case" to="/todo">
+                            TO DO
+                        </Link>
                     </div>
                 </div>
-                <div className="">
+                <div>
                     <AuthButton />
                 </div>
             </div>
-            <Outlet />
+            <div className="p-4">
+                <Outlet />
+            </div>
         </div>
     );
 };
